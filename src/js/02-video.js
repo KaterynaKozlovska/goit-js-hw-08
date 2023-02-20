09a6567c81555d39efaaa3e1171503b56e22b8f8
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-20 22:16:38 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,29 +3,16 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const currentTime = function (data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
-
-  let time = Number(localStorage.getItem('videoplayer-current-time'));
-  console.log(time);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
 };
 
 player.on('timeupdate', throttle(currentTime, 1000));
 
 player
-  .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+  .setCurrentTime(Number(localStorage.getItem(STORAGE_KEY)))
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
